6eb8dae1d447424e682eddbf6b2bee87b2ba226c
mtroiani <troiani.mary@gmail.com>
2018-05-17 12:35:57 -0700

    feat: Add lead jammer textView, set up buttons to toggle lead jammer.

--- score_keeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ score_keeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -15,35 +15,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void display(int score, int id) {
+    public void display(String msg, int id) {
         TextView scoreDisplay = (TextView) findViewById(id);
-        scoreDisplay.setText("" + score);
+        scoreDisplay.setText(msg);
     }
 
     public void addOneHomeTeam(View v) {
         scoreHomeTeam += 1;
-        display(scoreHomeTeam, R.id.home_score);
+        display(Integer.toString(scoreHomeTeam), R.id.home_score);
     }
 
     public void addFiveHomeTeam(View v) {
         scoreHomeTeam += 5;
-        display(scoreHomeTeam, R.id.home_score);
+        display(Integer.toString(scoreHomeTeam), R.id.home_score);
     }
 
     public void addOneAwayTeam(View v) {
         scoreAwayTeam += 1;
-        display(scoreAwayTeam, R.id.away_score);
+        display(Integer.toString(scoreAwayTeam), R.id.away_score);
     }
 
     public void addFiveAwayTeam(View v) {
         scoreAwayTeam += 5;
-        display(scoreAwayTeam, R.id.away_score);
+        display(Integer.toString(scoreAwayTeam), R.id.away_score);
     }
 
     public void resetBtn(View v) {
         scoreAwayTeam = 0;
         scoreHomeTeam = 0;
-        display(scoreAwayTeam, R.id.away_score);
-        display(scoreHomeTeam, R.id.home_score);
+        display(Integer.toString(scoreAwayTeam), R.id.away_score);
+        display(Integer.toString(scoreHomeTeam), R.id.home_score);
+        display("Lead Jammer: None", R.id.lead_jammer);
+    }
+
+    public void leadAway(View v) {
+        display("Lead Jammer: Away", R.id.lead_jammer);
+    }
+
+    public void leadHome(View v) {
+        display("Lead Jammer: Home", R.id.lead_jammer);
     }
 }
